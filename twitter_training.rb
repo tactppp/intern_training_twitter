78b1712dec4134a98621dd53f38e97139bfcd0bb
tact <tactppp@gmail.com>
2015-11-28 16:10:05 +0900

    ccc

---
+++ twitter_training.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+require 'dotenv'
+
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = 'SkYGENE0aGE8ubLMSk48EK8Q0'
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = '140300834-qZM9uKvSSMg8cVvME82M0UcclATMcbOATKX2iZuW'
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+client.update("test-tweet")
